10517d802da4320943c0078a0515f878c2995085
umutCan <umut.mkt@gmail.com>
2022-12-18 01:57:04 +0300

    some_libraries_added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Meteor } from "meteor/meteor";
+import { useNavigate } from "react-router-dom";
+import { useTracker } from "meteor/react-meteor-data";
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+import { Login } from "./Login.jsx";
+
+export const App = () => {
+  const navigate = useNavigate();
+
+  const user = useTracker(() => {
+    return Meteor.user();
+  });
+
+  useEffect(() => {
+    user && window.location.pathname === "/"
+      ? user.profile.role === "Manager"
+        ? navigate("/manager")
+        : user.profile.role === "Teacher"
+        ? navigate("/teacher")
+        : navigate("/student")
+      : {};
+  }, [user]);
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+    </div>
+  );
+};
---
+++ imports/ui/Login.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Meteor } from "meteor/meteor";
+
+export const Login = () => {
+  const [email, setEmail] = useState(() => "");
+  const [password, setPassword] = useState(() => "");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    Meteor.loginWithPassword(email, password, (err) => {
+      !err ? (setEmail(""), setPassword("")) : alert("Incorrect Credentials.");
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLogin} className="login-form">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          autoFocus
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
